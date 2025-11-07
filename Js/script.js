60fa7716796c5da54ae28afee860f38ea5a1a336
Mo Saleem <saleemmr7408@gmail.com>
2025-11-07 15:18:01 +0530

    first commit

---
+++ Js/script.js
@@ -0,0 +1,77 @@
+
+const codeLines = [
+"using System;",
+"using System.Collections.Generic;",
+"",
+"namespace Portfolio",
+"{",
+"    public class Developer",
+"    {",
+"        public string Name { get; set; } = \"Mo Saleem\";",
+"        public string Role { get; set; } = \"Full Stack .NET Developer\";",
+"        public string Location { get; set; } = \"Lucknow, India\";",
+"",
+"        public List<string> Skills { get; set; } = new List<string>",
+"        {",
+"            \"ASP.NET Core\",",
+"            \"C#\",",
+"            \"JavaScript\",",
+"            \"Bootstrap\",",
+"            \"SQL Server\"",
+"        };",
+"",
+"        public string Passion { get; set; } = \"Clean Code & Scalable Apps\";",
+"        public string Status { get; set; } = \"Available for Work\";",
+"    }",
+"}"
+];
+
+const codeEl = document.getElementById("code");
+const speed = 40;
+
+// Function to add syntax highlighting
+function highlight(line) {
+  return line
+    .replace(/(using|namespace|public|class|string|List|new)/g, '<span class="keyword">$1</span>')
+    .replace(/"([^"]*)"/g, '<span class="string">"$1"</span>')
+    .replace(/(Portfolio|Developer)/g, '<span class="type">$1</span>')
+    .replace(/(\/\/.*)/g, '<span class="comment">$1</span>');
+}
+
+let i = 0;
+
+function typeCode() {
+  if (i < codeLines.length) {
+    const coloredLine = highlight(codeLines[i]);
+    codeEl.innerHTML += coloredLine + "\n";
+    i++;
+    setTimeout(typeCode, speed * 3);
+  }
+}
+
+window.onload = typeCode;
+
+
+
+const scrollTop = document.getElementById("scroll-top");
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+        scrollTop.style.display = "block";
+    } else {
+        scrollTop.style.display = "none";
+    }
+};
+
+
+ // Toggle mobile menu
+  const toggle = document.getElementById('menu-toggle');
+  const navMenu = document.getElementById('nav-menu');
+
+  toggle.onclick = function() {
+    navMenu.classList.toggle('active');
+    toggle.classList.toggle('open');
+  };
+
+
+  
